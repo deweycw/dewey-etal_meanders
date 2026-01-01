2b2bf99f2b83997f954c5e6b4be601a420835c92
Christian Dewey <dewey.christian@gmail.com>
2026-01-01 12:33:19 -0600

    added 2018 dirs for both meanders

---
+++ pflotran/simulations/mcp18/mcp-grid/pflo.py
@@ -0,0 +1,276 @@
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt 
+import scipy
+import csv
+import glob
+import shutil
+from datetime import datetime, date, time
+from h5py import *
+import h5py
+import re
+import matplotlib.pyplot as plt
+from io import StringIO   # StringIO behaves like a file object
+import math
+'''
+THIS MODULE CONTAINS FUNCTIONS POTENTIALLY USEFULF FOR MODIFYING DATA FOR PFLOTRAN SIMULATIONS
+'''
+
+def get_distance(list1, list2):
+	'''
+	This function finds the distance between points in a matrix. The coordinates are the row and col indices. 
+
+	list1:  {numpy array}  [[row_1a,col_1a],...,[row_na,col_na]] np array of points
+	list2:  {numpy array}  [[row_1b,col_1b],...,[row_nb,col_nb]] np array of points
+
+	returns: {list}: list of points and distances assoviated wtih points
+				e.g. 
+					[[([x1,y1], [x2,y2]),d1] 
+	   				 [([x1,y1], [x3,y3]),d2]]
+	   			where [x1,y1] is the ref point
+	'''
+
+	len1 = len(list1)
+	len2 = len(list2)
+	distance = []
+
+	for l1 in range(0,len1):
+		for l2 in range(0,len2):
+			distance.append([([list1[l1][0],list1[l1][1]], [list2[l2][0],list2[l2][1]]), math.sqrt((list1[l1][0]-list2[l2][0])**2 + (list1[l1][1]-list2[l2][1])**2)])
+
+	return distance
+
+
+ 
+def make_pflo_mat(vec,dim):
+	'''
+	This function takes a 1D vector and converts it to a matrix of specified size. 
+	For a matrix M of size (r,c), elements are added as follows:
+
+		M[r][0], M[r][1], ... , M[r][c-1], M[r][c]
+		M[r-1][0], M[r-1][1], ... , M[r-1][c-1], M[r-1][c]
+		.
+		.
+		.
+		M[1][0], M[1][1], ... , M[1][c-1], M[1][c]
+		M[0][0], M[0][1], ... , M[0][c-1], M[0][c]
+
+	Population is performed in this way so that material ID vectors for PFLOTRAN can be tested for correct order. 
+
+	vec:     {numpy.array}  1D array (vertical)
+	dim:     {tuple}        tuple with specified dimensions of target matrix (rows, cols); user needs to insure that dimensions are possible for 'vec'
+	return:  {numpy.array}  2D array with shape = dim
+	'''
+
+	i = 0
+	rows = dim[0]
+	cols = dim[1]
+	mat = np.zeros(dim)
+	for rr in range(rows,0,-1):
+		for cc in range(0,cols):
+			mat[rr-1,cc] = vec[i]
+			i = i + 1
+	mat = mat.astype('float')	
+	return mat
+
+def make_3d_mat(vec,dim):
+    '''
+    This function takes a 1D vector and converts it to a 3D matrix of specified size. 
+    For a matrix M of size (r,c), elements are added as follows:
+
+        M[r][0], M[r][1], ... , M[r][c-1], M[r][c]
+        M[r-1][0], M[r-1][1], ... , M[r-1][c-1], M[r-1][c]
+        .
+        .
+        .
+        M[1][0], M[1][1], ... , M[1][c-1], M[1][c]
+        M[0][0], M[0][1], ... , M[0][c-1], M[0][c]
+
+    Population is performed in this way so that material ID vectors for PFLOTRAN can be tested for correct order. 
+
+    vec:     {numpy.array}  1D array (vertical)
+    dim:     {tuple}        tuple with specified dimensions of target matrix (rows, cols, nz); user needs to insure that dimensions are possible for 'vec'
+    return:  {numpy.array}  3D array with shape = dim
+    '''
+
+    cc = 0
+    rows = dim[0]
+    cols = dim[1]
+    nz = dim[2]
+    mat = np.zeros(dim)
+    for jj in range(0,cols):    
+        for kk in range(nz,0,-1):    
+            for ii in range(rows,0,-1):
+                mat[ii-1,jj,kk-1] = vec[cc]
+                cc += 1
+    mat = mat.astype('float')   
+    return mat
+
+# Region object
+class Region:
+  def __init__(self,region_group,region_name,filename):
+    self.region_name = region_name
+    self.filename = filename
+    self.group = region_group.create_group(self.region_name)
+  def writeRegion(self,n):
+    # in text file, region data should be specified as follows
+    # cell_id face_id
+    # one connection per line
+    cell_id_array = np.zeros(n,'=i4')
+    face_id_array = np.zeros(n,'=i4')
+    f = open(self.filename)
+    count = 0
+    while (1):
+      s = f.readline()
+      if len(s) < 2:
+        break
+      w = s.split()
+      cell_id_array[count] = int(float(w[0]))
+      face_id_array[count] = int(float(w[1]))
+      count += 1
+    iarray = np.zeros(count,'=i4')
+    iarray[0:count] = cell_id_array[0:count]
+    dataset_name = 'Cell Ids'
+    print('i: ',np.shape(iarray))
+    self.group.create_dataset(dataset_name, data=iarray)
+    cell_id_array = 0
+    iarray[0:count] = face_id_array[0:count]
+    dataset_name = 'Face Ids'
+    self.group.create_dataset(dataset_name, data=iarray)
+    face_id_array = 0
+    iarray = 0
+    print('done with Region:', self.region_name)
+
+
+
+def assign_inactive(elev,domain):
+    '''
+    elev: float specifying elevation criterion for assignment of active cells; cells with elevation below this criterion will be inactive
+    domain: 2D matrix of DEM data 
+    
+    returns:
+    '''
+
+    nx = np.shape(domain)[1] # number of cols is number of cells in X-dir
+    ny = np.shape(domain)[0] # number of rows is number of cells in Y-dir
+    mat_id  = np.ones((ny,nx))  # matrix of cell ids; same size as domain
+    print(np.shape(mat_id))
+    mat_id= mat_id.astype('int')
+
+    for xx in range(0,nx):
+        for yy in range(0,ny):
+            if domain[yy,xx] < elev:
+                mat_id[yy,xx] = 0
+    return mat_id
+
+
+
+def deactivate_range(points,dir,mat_id):
+    '''Given a two points that define a line cutting the domain, assign values of 0 to all cells above or below the line 
+    line: two points in domain [[x1,y1],[x2,y2]], where x2 is greater than x1 (y1, y2 are the Y-coords associated with those X-coords); NOTE: origin is top-left --> Y increases downward
+    dir: {string} 'above' or 'below'; 1 will deactivate above line; 0 deactivates below line 
+    mat_id: matrix of material ids, same size as domain 
+    return: matrix with deactivated cells 
+    '''
+    x1 = points[0][0]
+    x2 = points[1][0]
+    y1 = points[0][1]
+    y2 = points[1][1]
+    nx = np.absolute(x2 - x1)
+    ny = np.absolute(y2 - y1)
+    yl = np.shape(mat_id)[0]
+    m = -(y2-y1)/(x2-x1)
+    m = np.ceil(1/m)
+    sign = np.absolute(m)/m
+    dstep = 1
+    dy = 0
+    if dir is 'above':
+        for xx in range(x1,x2):
+            for yy in range(0, int(y1 + dy)):
+                mat_id[yy,xx] = 0
+            if  dstep % m == 0:
+                dy = dy - sign
+            dstep = dstep + 1
+    elif dir is 'below':
+        for xx in range(x1,x2):
+            for yy in range(int(y1 + dy),yl):
+                mat_id[yy,xx] = 0
+            if  dstep % m == 0:
+                dy = dy - sign
+            dstep = dstep + 1
+    return mat_id
+
+def assign_BC_cells(mat_id, idn):
+    '''
+    assigns cells around active cells an ID which can be used to define the boundary condition zone
+
+    mat_id: matrix of material ids, same dims as domain 
+    idn: {int} integer to be used as cell ID
+    return: [matrix of cell IDs with BC cells identified, matrix of face vals for BCs,array of coords of BC cells]
+    '''
+    nx = np.shape(mat_id)[1]
+    ny = np.shape(mat_id)[0]
+    faces = np.zeros_like(mat_id)
+    coords = []
+    cellid = []
+
+    for xx in range(0, int(nx)):
+        for yy in range(0, int(ny)):
+            if mat_id[yy,xx] == 1 and mat_id[yy-1,xx] == 0: # inactive cells above
+                if yy == 0:     
+                    mat_id[yy,xx] = idn+2
+                    faces[yy,xx] = 3 #south face
+                    coords.append([yy,xx,1])
+                    #cellid.append([nx*ny])
+                else:
+                    mat_id[yy-1,xx] = idn
+                    faces[yy-1,xx] = 4 #north face
+                    coords.append([yy-1,xx,1])
+                    #cellid.append([xx*yy])
+            if mat_id[yy,xx] == 0 and mat_id[yy-1,xx] == 1: # inactive cells below
+                mat_id[yy,xx] = idn
+                faces[yy,xx] = 3 #south face
+                coords.append([yy,xx,1])
+                #cellid.append([xx*yy])
+            if mat_id[yy,xx] == 1 and mat_id[yy,xx-1] == 0: # inactive cells to left
+                if xx == 0: 
+                    mat_id[yy,xx] = idn+2
+                    faces[yy,xx] = 2 #east face
+                    coords.append([yy,xx,1])
+                    #cellid.append([nx*ny])
+                else:
+                    mat_id[yy,xx-1] = idn
+                    faces[yy,xx-1] = 1 #west face
+                    coords.append([yy,xx-1,1])
+                    #cellid.append([xx*yy])
+            if mat_id[yy,xx] == 0 and mat_id[yy,xx-1] == 1: # inactive cells to right
+                mat_id[yy,xx] = idn
+                faces[yy,xx] = 2 #east face
+                coords.append([yy,xx,1])
+                #cellid.append([xx*yy]) 
+    coords = np.asarray(coords)
+    cellid = np.asarray(cellid)
+    return mat_id, faces, coords 
+
+def find_cells(mat1d,eq,val):
+    '''
+    finds cell IDs of  specific val
+    mat1d: list of material IDs (reshaped matrix)
+    eq: <string> "equal" or "not" --> determines whether to search for values equal to val or not equal to val
+    returns: np array of indices of cells with val
+    '''
+    if eq is "equal":
+        cells = []
+        for i in range(0,np.shape(mat1d)[0]):
+            if mat1d[i] == val:
+                cells.append(i)
+        cells = np.asarray(cells)
+        
+    else:
+        cells = []
+        for i in range(0,np.shape(mat1d)[0]):
+            if mat1d[i] != val:
+                cells.append(i)
+        cells = np.asarray(cells)
+    return cells  
---
+++ pflotran/simulations/mzt18/mzt-grid/str_grid-2-MZT-top-bc.py
@@ -0,0 +1,220 @@
+from h5py import *
+import numpy as np
+import matplotlib.pyplot as plt
+import pflo
+
+'''
+
+Provide a data file including DEM in a column or row 
+
+124 cells in length for mcp
+
+
+works for x-sections
+'''
+
+def create_structred_grids(zz, dz, overall_domain_depth, bgs_soil):  #  resolution, zz -- elevation or DEM
+    ny = 1
+    nx = np.shape(zz)[0]
+    overtop_null = 1.
+    zz_size = np.shape(zz)[0]
+    zz_max = np.floor(np.max(zz[1:zz_size]))+overtop_null # zz_size include cells for BC that are not used for max/min calculation
+    zz_min = zz_max - overall_domain_depth # np.floor(np.min(zz[1:zz_size]))-(overall_domain_depth-overtop_null)
+    zz_col = np.arange(zz_min, zz_max+dz, dz)
+    nz = np.size(zz_col)
+
+    gs_ref = np.mean(zz[:])
+    
+    #bgs_weathered_shale = 3.4
+    print("\nref: ", gs_ref, "\nmax: ", zz_max, "\nmin: ", zz_min, "\nsoil: ",gs_ref - bgs_soil ) #,"\nshale: ",gs_ref - bgs_weathered_shale)
+
+    mat_id  = np.ones((nx,nz))
+    mat_id= mat_id.astype('int')
+    mat_id_1d = np.reshape(mat_id,(nx*nz,1), order="F")
+
+    bc_id  = np.ones((nx,nz))
+    bc_id= bc_id.astype('int')
+    bc_id_1d = np.reshape(bc_id,(nx*nz,1), order="F")
+
+    face_id  = np.ones((nx,nz))
+    face_id= face_id.astype('int')
+    face_id_1d = np.reshape(face_id,(nx*nz,1), order="F")
+
+    cell_id = np.reshape(np.arange(1, (nx*nz) +1),(nx,nz),order="F")
+    cell_id= cell_id.astype('int')
+    cell_id_1d = np.reshape(cell_id,(nx*nz, ),order="F")
+
+    itop = 0
+    for ii  in range(0, nx):  
+        if ii < 1:
+            # upstream bc
+            bank_top = nz #int(1.7 / dz)  
+            bc_id[ii,0:bank_top] = 8 # assign upstream BC cells
+            face_id[ii,0:bank_top] = 3 # 
+            #mat_id[ii,bank_top:np.size(zz_col)] = 0
+
+        elif ii >= nx-1: 
+            # downstream bc
+            bank_top = nz #int(1.6/ dz)   
+            bc_id[ii,0:bank_top] = 9 # assign downtstream (north) BC cells
+            face_id[ii,0:bank_top] = 4 # assign west face to downstream BC 
+            #mat_id[ii,bank_top:np.size(zz_col)] = 0
+        
+        for  kk  in range(0, nz):
+            if (zz_col[kk] - zz[ii] >= 0): # if above ground surface
+                mat_id[ii, kk:np.size(zz_col)] = 0
+                break
+            elif (zz_col[kk] - zz[ii] <=0) and (zz_col[kk] - zz[ii] >= -0.1): # if at surface top layer
+                kk_surface = int(kk + np.floor(0.1/dz))
+                bc_id[ii, kk] = 91 + itop
+                mat_id[ii, kk] = 2
+                face_id[ii,kk] = 6 # assign top face to top US BC 
+            elif ((zz_col[kk] - zz[ii] < 0) and (zz_col[kk] - (gs_ref - bgs_soil) > 0)): # if in soil layer 
+                kk_surface = int(kk + np.floor(bgs_soil/dz))
+                mat_id[ii, kk:kk_surface] = 2
+        itop = itop + 1 
+
+    mat_id_1d =  np.reshape(mat_id,(nx*nz, ),order="F")
+    bc_id_1d =  np.reshape(bc_id,(nx*nz, ),order="F")
+    face_id_1d =  np.reshape(face_id,(nx*nz, ),order="F")
+
+    return mat_id_1d, cell_id_1d, face_id_1d,bc_id_1d,  nx, ny, nz, zz_min
+
+
+def write_regions_files(mat_id_1d, cell_id_1d,  face_id_1d, bc_id_1d):
+    # find the indices of the BC cells
+    # upstream BC 
+    upstream_bc_1d = pflo.find_cells(bc_id_1d,"equal",8)
+    upstream_bc_faces_1d = face_id_1d[upstream_bc_1d.astype('int')][...,None]
+    upstream_bc_ids = cell_id_1d[upstream_bc_1d.astype('int')][...,None]
+
+    # TOP  BCs
+    top_bc_1d_list = [pflo.find_cells(bc_id_1d,"equal",91 + itop) for itop in range(0,nx)]
+    top_bc_faces_1d_list = [face_id_1d[top_bc_1d.astype('int')][...,None] for top_bc_1d in top_bc_1d_list]
+    top_bc_ids_list= [cell_id_1d[top_bc_1d.astype('int')][...,None] for top_bc_1d in top_bc_1d_list]
+
+    # downstream BC 
+    downstream_bc_1d = pflo.find_cells(bc_id_1d,"equal",9)
+    downstream_bc_faces_1d = face_id_1d[downstream_bc_1d.astype('int')][...,None]
+    downstream_bc_ids = cell_id_1d[downstream_bc_1d.astype('int')][...,None]
+
+    print("upstream_bc_ids: ", np.shape(upstream_bc_ids))
+    print("downstream_bc_ids: ", np.shape(downstream_bc_ids))
+    #print("top_bc_ids: ", np.shape(top_bc_ids))
+
+    # create regions for .hd5 Region/Materials file 
+    upstream_bc = np.append(upstream_bc_ids,upstream_bc_faces_1d, axis = 1)
+    np.savetxt('upstream_bc_reg.txt',upstream_bc,delimiter = ' ')
+    downstream_bc = np.append(downstream_bc_ids,downstream_bc_faces_1d, axis = 1)
+    np.savetxt('downstream_bc_reg.txt',downstream_bc,delimiter = ' ')
+
+    itop = 0
+    for top_bc_ids, top_bc_faces_1d in zip(top_bc_ids_list, top_bc_faces_1d_list):
+        top_bc = np.append(top_bc_ids,top_bc_faces_1d, axis = 1)
+        fname = f'top_bc_reg_{itop}.txt'
+        np.savetxt(fname,top_bc,delimiter = ' ')     
+        itop = itop + 1  
+
+    #soil reg
+    soil_1d = pflo.find_cells(mat_id_1d,"equal",2)
+    soil_faces_1d = face_id_1d[soil_1d.astype('int')][...,None]
+    soil_ids = cell_id_1d[soil_1d.astype('int')][...,None]
+
+    #gravel reg
+    gravel_1d = pflo.find_cells(mat_id_1d,"equal",1)
+    gravel_faces_1d = face_id_1d[gravel_1d.astype('int')][...,None]
+    gravel_ids = cell_id_1d[gravel_1d.astype('int')][...,None]
+
+
+    print("soil_ids: ", np.shape(soil_ids))
+    print("gravel_ids: ", np.shape(gravel_ids))
+    #print("shale_ids: ", np.shape(shale_ids))
+
+    soil = np.append(soil_ids,soil_faces_1d, axis = 1)
+    np.savetxt('soil_reg.txt',soil, delimiter = ' ')
+    gravel = np.append(gravel_ids,gravel_faces_1d, axis = 1)
+    np.savetxt('gravel_reg.txt',gravel,delimiter = ' ')
+
+
+
+    h5_file_name = "../xxgrid010-new.h5"  #'datasets.h5'
+    h5file = File(h5_file_name, mode ='w')
+
+    dataset_name = "/Materials/Cell Ids"
+    h5dset = h5file.create_dataset(dataset_name, data = cell_id_1d, dtype=np.uint64)
+
+    dataset_name = "/Materials/Material Ids"
+    h5dset = h5file.create_dataset(dataset_name, data = mat_id_1d, dtype=np.uint64)
+
+    region_group = h5file.create_group("Regions")
+
+    n = np.shape(upstream_bc_faces_1d)[0]
+    print(n)
+    filename = 'upstream_bc_reg.txt'
+    region_name = filename.split('.')[0]
+    region = pflo.Region(region_group,region_name,filename)
+    region.writeRegion(n)
+
+    itop = 0
+    for top_bc_faces_1d in top_bc_faces_1d_list:
+        n = np.shape(top_bc_faces_1d)[0]
+        print(n)
+        filename = f'top_bc_reg_{itop}.txt'
+        region_name = filename.split('.')[0]
+        print(region_name)
+        region = pflo.Region(region_group,region_name,filename)
+        region.writeRegion(n)
+        itop = itop + 1
+
+    n = np.shape(downstream_bc_faces_1d)[0]
+    print(n)
+    filename = 'downstream_bc_reg.txt'
+    region_name = filename.split('.')[0]
+    region = pflo.Region(region_group,region_name,filename)
+    region.writeRegion(n)
+
+
+    n = np.shape(soil_faces_1d)[0]
+    print(n)
+    filename = 'soil_reg.txt'
+    region_name = filename.split('.')[0]
+    region = pflo.Region(region_group,region_name,filename)
+    region.writeRegion(n)
+
+    n = np.shape(gravel_faces_1d)[0]
+    print(n)
+    filename = 'gravel_reg.txt'
+    region_name = filename.split('.')[0]
+    region = pflo.Region(region_group,region_name,filename)
+    region.writeRegion(n)
+
+
+    h5file.close()
+
+if __name__ == "__main__":
+
+    DEM = np.loadtxt('MZT2.csv',delimiter = ',')
+    DEM = np.reshape(DEM,(np.size(DEM),1), order="F")
+
+    print(DEM)
+
+    bgs_soil = 0.2 #m
+    dz = 0.10
+
+    overall_domain_depth = 2.5 #m
+
+    [mat_id_1d, cell_id_1d,  face_id_1d, bc_id_1d, nx, ny, nz, zz_min] = create_structred_grids(DEM, dz, overall_domain_depth, bgs_soil)
+
+    write_regions_files(mat_id_1d, cell_id_1d,  face_id_1d, bc_id_1d)
+
+    n_inactive = np.count_nonzero(mat_id_1d == 0)
+    n_active = np.count_nonzero(mat_id_1d != 0)
+
+    print(f"Domain dimensions (ny, nx, nz): {ny, nx, nz}")  
+
+    print(f'Total # cells: {ny*nx*nz}')
+
+    print(f"Total # ACTIVE cells: {n_active}")
+
+    print(f"Total # INACTIVE cells: {n_inactive}")
+
